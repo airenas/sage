726ac097f71ba36ae5b873c76712698a7464c134
Airenas Vaičiūnas <airenass@gmail.com>
2022-08-07 15:17:38 +0300

    Return svg image bytes, test colors

--- sage/latex/wrapper.py
+++ sage/latex/wrapper.py
@@ -11,10 +11,11 @@ class LatexWrapper:
 
     def prepare(self, txt: str) -> bytes:
         # curl -X POST localhost:5030/renderLatex -H "content-type:application/json" -d '{"content":"\\begin{math} \\frac{2 + 2}{6} \\end{math}","latexPackages":[],"output":"svg"}'  -o out.svg
-        eq = "\\begin{math} %s \\end{math}" % txt
-        in_data = {'content': eq, 'latexPackages': [], 'output': 'svg'}
+        eq = "\\pagecolor{gray}\n\\color{white}\n\\begin{math} %s \\end{math}" % txt
+        in_data = {'content': eq, 'latexPackages': ['\\usepackage{xcolor}'], 'output': 'svg'}
         x = requests.post(self.__url, json=in_data)
         if x.status_code != 200:
             raise Exception("Can't prepare equation image")
-        data = x.text
-        return data
+        return x.text
+        # data = "\n".join(data.split("\n")[1:])
+        # return bytes(data, 'utf-8')
